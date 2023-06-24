9a8e398c83a2451b28bc23b432ecb605dcd28964
Shorouk Hatem <60854152+shoroukhatem@users.noreply.github.com>
2023-06-25 01:40:42 +0300

    25/6
    All functions works

--- frontend.js
+++ frontend.js
@@ -1,70 +1,46 @@
-// Example code to update the values (replace with your actual implementation)
-const tempData = [];
-const humData = [];
-const moisData = [];
 
-axios.get('http://localhost:5000/soil-moisture')
-.then(response => {
-  // handle success
-  moistureData = response.data.soilmoisture
-  console.log(response.data.soilmoisture);
-})
-.catch(error => {
-  // handle error
-  console.log(error);
-});
 
-axios.get('http://localhost:5000/temperature')
-.then(response => {
-  // handle success
-  temperatureData = response.data.temperature
-  console.log(response.data.temperature);
-})
-.catch(error => {
-  // handle error
-  console.log(error);
-});
-
-  axios.get('http://localhost:5000/humidity')
-.then(response => {
-  // handle success
-  humidityData = response.data.humidity
-  console.log(response.data.humidity);
-})
-.catch(error => {
-  // handle error
-  console.log(error);
-});
-  axios.get('http://localhost:5000/actoatorState')
-.then(response => {
-  // handle success
-  updateActuatorStatus(response.data.state);  
-  console.log(response.data.state);  
-})
-.catch(error => {
-  // handle error
-  console.log(error);
-});
-
-function updateSensorData() {
-// Fetch and update temperature value
-const temperatureValue = temperatureData // Replace with actual temperature value
-document.getElementById('temperature-value').textContent = temperatureValue.toFixed(2);
-tempData.push(temperatureValue); // Store temperature value
-
-// Fetch and update humidity value
-const humidityValue = humidityData // Replace with actual humidity value
-document.getElementById('humidity-value').textContent = humidityValue.toFixed(2);
-humData.push(humidityValue); // Store humidity value
-
-// Fetch and update moisture value
-const moistureValue = moistureData// Replace with actual moisture value
-document.getElementById('moisture-value').textContent = moistureValue.toFixed(2);
-moisData.push(moistureValue); // Store moisture value
-
+async function fetchTemperatureData() {
+  try {
+    const response = await axios.get("http://localhost:5000/temperature"); // Replace "/api/get-value" with your backend endpoint
+    const temperatureData = response.data.temperature; // Assuming the response contains a "value" property
+    // updatetemperatureData(temperatureValue);
+    const valueElement = document.getElementById("temperature-value");
+    valueElement.textContent = temperatureData;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
+async function fetchHumidityData() {
+  try {
+    const response = await axios.get("http://localhost:5000/humidity"); // Replace "/api/get-value" with your backend endpoint
+    const humidityData = response.data.humidity; // Assuming the response contains a "value" property
+    // updatehumidityData(value);
+    const valueElement = document.getElementById("humidity-value");
+    valueElement.textContent = humidityData;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
+async function fetchMoistureData() {
+  try {
+    const response = await axios.get("http://localhost:5000/soil-moisture"); // Replace "/api/get-value" with your backend endpoint
+    const moistureData = response.data.soilmoisture; // Assuming the response contains a "value" property
+    // updateValue2(value);
+    const valueElement = document.getElementById("moisture-value");
+    valueElement.textContent = moistureData;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function updateSensorData() {
+  fetchMoistureData();
+  fetchHumidityData();
+  fetchTemperatureData();
+}
 
 // Attach an event listener to the threshold input element
 function updateThresholdValue() {
@@ -89,35 +65,33 @@ axios.patch("http://localhost:5000/pumpThreshold",data,{ headers } )
 document.getElementById('threshold-input').addEventListener('input', updateThresholdValue);
 
 
-// Function to check if the current moisture value exceeds the threshold
-function checkMoistureThreshold() {
-const moistureValue = parseFloat(document.getElementById('moisture-value').textContent);
-const thresholdValue = parseFloat(document.getElementById('threshold-input').value);
-
-if (moistureValue > thresholdValue) {
-// Perform action when threshold is exceeded
-console.log('Moisture threshold exceeded!');
-// Add your custom logic here
-}
-}
-
-// Call the function to check the moisture threshold initially
-checkMoistureThreshold();
-
 // Call the function to update moisture sensor data initially
 
 // Function to update the actuator status display
-function updateActuatorStatus(status) {
-const actuatorStatus = document.getElementById('actuator-status');
-
-// Clear previous classes
-actuatorStatus.classList.remove('actuator-on', 'actuator-off');
-
+async function updateActuatorStatus(status) {
+const valueElement = document.getElementById('actuator-status');
+valueElement.classList.remove('actuator-on', 'actuator-off');
 // Update status text and apply corresponding class
-actuatorStatus.classList.add(status ? 'actuator-on' : 'actuator-off');
+valueElement.classList.add(status? 'actuator-on' : 'actuator-off');
+}
+async function ActuatorStatus(){
+  var status = false;
+  const response = await axios.get('http://localhost:5000/actoatorState');
+  const actoatorStatus = response.data.state;
+  if (actoatorStatus == "on"){
+     status = true;
+  }
+  updateActuatorStatus(status);
 }
 
-// Attach an event listener to the toggle actuator button
+function liveData(){
+  setInterval(function(){
+    updateSensorData();
+    ActuatorStatus();
+  },3000)
+}
 
-// Call the function to update the actuator status initially
-updateActuatorStatus(false); // Set initial status to "Off"
+document.addEventListener('DOMContentLoaded',function(){
+  liveData();
+});
+
